c3df61f263749a0a1feadf8d88cfaaf222ad9044
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-03-28 15:09:13 -0300

    Lets get rusty - RefCell Smart Pointer

---
+++ lets-get-rusty/22-ref-cell-smart-pointer/src/main.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::{rc::Rc, cell::RefCell};
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil
+}
+
+use crate::List::{Cons, Nil};
+
+fn main()
+{
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
